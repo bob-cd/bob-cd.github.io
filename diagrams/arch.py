719b068e3a8bce038e7a3caf4da53938e85aadef
Rahul De <rahul080327@gmail.com>
2023-03-06 13:31:06 +0000

    [housekeeping] better diagram

--- diagrams/arch.py
+++ diagrams/arch.py
@@ -1,13 +1,12 @@
 from diagrams import Cluster, Diagram
 from diagrams.aws.compute import EC2
-from diagrams.aws.integration import SimpleQueueServiceSqsQueue
 from diagrams.aws.network import ALB
-from diagrams.aws.database import RDSPostgresqlInstance
+from diagrams.custom import Custom
 
 if __name__ == "__main__":
     with Diagram("Bob Cluster", show=False, direction="TB"):
-        rabbitmq = SimpleQueueServiceSqsQueue("RabbitMQ")
-        db = RDSPostgresqlInstance("XTDB")
+        rabbitmq = Custom("Queue", "rabbitmq.png")
+        db = Custom("Database", "xt.png")
 
         with Cluster("API Servers"):
             api_servers = [EC2("server1"), EC2("server2"), EC2("server3")]
